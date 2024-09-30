593c55db07bda8253832472da32af74f5d231aa7
Xen1k <xen1kdevelop@gmail.com>
2024-09-30 22:22:41 +0300

    Server is up route

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 app.register_blueprint(qr_blueprint, url_prefix="/qr")
 cors = CORS(app)
 
+
+@app.route('/', methods=['GET'])
+def handle_find_qr_message():
+    return 'RuScan server is up'
+
 if __name__ == '__main__':
     app.run(debug=True, host='192.168.1.111', port=5000)
